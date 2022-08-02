738fe6ca8e2841208f59bcd37848bc294461267c
Lance <lancelancezhang@gmail.com>
2022-08-02 17:14:40 +1200

    Initial commit with dropdown

--- app/src/main/java/com/example/factapplication/BullseyeActivity.java
+++ app/src/main/java/com/example/factapplication/BullseyeActivity.java
@@ -1,14 +1,52 @@
 package com.example.factapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class BullseyeActivity extends AppCompatActivity {
+public class BullseyeActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(BullseyeActivity.this,
+                android.R.layout.simple_spinner_item,getResources().getStringArray(R.array.values_array));
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+
     }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+        switch (position) {
+            case 0:
+                // Whatever you want to happen when the first item gets selected
+                break;
+            case 1:
+                // Whatever you want to happen when the second item gets selected
+                break;
+            case 2:
+                // Whatever you want to happen when the thrid item gets selected
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        // TODO Auto-generated method stub
+    }
+
+
+
 }
--- app/src/main/java/com/example/factapplication/MenuActivity.java
+++ app/src/main/java/com/example/factapplication/MenuActivity.java
@@ -2,16 +2,44 @@ package com.example.factapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class MenuActivity extends AppCompatActivity {
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        final RelativeLayout rr = (RelativeLayout) findViewById(R.id.rr);
+        rr.setOnTouchListener(new View.OnTouchListener() {
+
+            @SuppressLint("UseCompatLoadingForDrawables")
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    int x = (int) event.getX();
+                    int y = (int) event.getY();
+                    RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
+                            RelativeLayout.LayoutParams.WRAP_CONTENT,
+                            RelativeLayout.LayoutParams.WRAP_CONTENT);
+                    ImageView iv = new ImageView(getApplicationContext());
+                    lp.setMargins(x, y, 0, 0);
+                    iv.setLayoutParams(lp);
+                    iv.setImageDrawable(getResources().getDrawable(
+                            androidx.constraintlayout.widget.R.drawable.abc_ab_share_pack_mtrl_alpha));
+                    ((ViewGroup) v).addView(iv);
+                }
+                return false;
+            }
+        });
     }
 
     public void goToChoicepoint(View view) {
