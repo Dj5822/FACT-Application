3097b977326947db680334113ebdada0a6b96f51
Dylan Jung <Dj5822@gmail.com>
2022-08-02 17:53:35 +1200

    Fixed activity switching
    Previously, we switched the view instead of switching activities. This is now fixed so that the code in each activity actually works.

--- app/src/main/java/com/example/factapplication/BullseyeActivity.java
+++ app/src/main/java/com/example/factapplication/BullseyeActivity.java
@@ -9,44 +9,19 @@ import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class BullseyeActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+public class BullseyeActivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.activity_bullseye);
+
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(BullseyeActivity.this,
-                android.R.layout.simple_spinner_item,getResources().getStringArray(R.array.values_array));
-
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        String[] items = new String[]{"value1", "value2", "value3"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
         spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
 
 
     }
-
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
-        switch (position) {
-            case 0:
-                // Whatever you want to happen when the first item gets selected
-                break;
-            case 1:
-                // Whatever you want to happen when the second item gets selected
-                break;
-            case 2:
-                // Whatever you want to happen when the thrid item gets selected
-                break;
-        }
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-        // TODO Auto-generated method stub
-    }
-
-
-
 }
--- app/src/main/java/com/example/factapplication/MenuActivity.java
+++ app/src/main/java/com/example/factapplication/MenuActivity.java
@@ -8,16 +8,37 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 public class MenuActivity extends AppCompatActivity {
+    Button switchToBullseye;
+    Button switchToChoicepoint;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        switchToBullseye = findViewById(R.id.bullseyeButton);
+        switchToBullseye.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToBullseye();
+            }
+        });
+
+        switchToChoicepoint = findViewById(R.id.choicePointButton);
+        switchToChoicepoint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToChoicepoint();
+            }
+        });
+
+        // Code related to bullseye testing.
         final RelativeLayout rr = (RelativeLayout) findViewById(R.id.rr);
         rr.setOnTouchListener(new View.OnTouchListener() {
 
@@ -42,11 +63,13 @@ public class MenuActivity extends AppCompatActivity {
         });
     }
 
-    public void goToChoicepoint(View view) {
-        setContentView(R.layout.activity_choicepoint);
+    public void goToChoicepoint() {
+        Intent switchActivityIntent = new Intent(this, ChoicepointActivity.class);
+        startActivity(switchActivityIntent);
     }
 
-    public void goToBullseye(View view) {
-        setContentView(R.layout.activity_bullseye);
+    public void goToBullseye() {
+        Intent switchActivityIntent = new Intent(this, BullseyeActivity.class);
+        startActivity(switchActivityIntent);
     }
 }
