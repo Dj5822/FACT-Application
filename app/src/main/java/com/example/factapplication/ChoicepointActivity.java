780f3de76078228e29c5b58c5cb2a97219802c6d
Dylan Jung <Dj5822@gmail.com>
2022-08-14 01:48:51 +1200

    Left and right swipes now work

--- app/src/main/java/com/example/factapplication/ChoicepointActivity.java
+++ app/src/main/java/com/example/factapplication/ChoicepointActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,13 +13,22 @@ public class ChoicepointActivity extends AppCompatActivity {
     private ViewHolder vh;
     private String position;
 
+    private final String leftChoice = "Buy whiskey";
+    private final String rightChoice = "Keep walking";
+    private final String scenario = "You come across the liquor store";
+
     private class ViewHolder {
         ImageView backgroundImage;
-        View choicepointView;
+        View choicePointView;
+        TextView scenarioText, currentText, leftText, rightText;
 
         public ViewHolder() {
             backgroundImage = findViewById(R.id.choicePointBackground);
-            choicepointView = findViewById(R.id.choicePointView);
+            choicePointView = findViewById(R.id.choicePointView);
+            scenarioText = findViewById(R.id.choicePointScenarioText);
+            currentText = findViewById(R.id.choicepointCurrentText);
+            leftText = findViewById(R.id.choicepointLeftSideText);
+            rightText = findViewById(R.id.choicepointRightSideText);
         }
     }
 
@@ -26,11 +36,11 @@ public class ChoicepointActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choicepoint);
-        this.position = "center";
-
         vh = new ViewHolder();
 
-        vh.choicepointView.setOnTouchListener(new OnSwipeTouchListener(ChoicepointActivity.this) {
+        initialize();
+
+        vh.choicePointView.setOnTouchListener(new OnSwipeTouchListener(ChoicepointActivity.this) {
             public void onSwipeLeft() {
                 if (position == "center") {
                     goToRightScreen();
@@ -52,18 +62,39 @@ public class ChoicepointActivity extends AppCompatActivity {
         });
     }
 
+    private void initialize() {
+        position = "center";
+        vh.scenarioText.setText(scenario);
+        vh.leftText.setText(leftChoice);
+        vh.rightText.setText(rightChoice);
+        vh.currentText.setVisibility(View.INVISIBLE);
+        vh.leftText.setVisibility(View.VISIBLE);
+        vh.rightText.setVisibility(View.VISIBLE);
+    }
+
     private void goToRightScreen() {
         position = "right";
+        vh.currentText.setText(rightChoice);
+        vh.currentText.setVisibility(View.VISIBLE);
+        vh.leftText.setVisibility(View.INVISIBLE);
+        vh.rightText.setVisibility(View.INVISIBLE);
         vh.backgroundImage.setImageResource(R.drawable.choice_point_background_right);
     }
 
     private void goToLeftScreen() {
         position = "left";
+        vh.currentText.setText(leftChoice);
+        vh.currentText.setVisibility(View.VISIBLE);
+        vh.leftText.setVisibility(View.INVISIBLE);
+        vh.rightText.setVisibility(View.INVISIBLE);
         vh.backgroundImage.setImageResource(R.drawable.choice_point_background_left);
     }
 
     private void goToCenterScreen() {
         position = "center";
+        vh.currentText.setVisibility(View.INVISIBLE);
+        vh.leftText.setVisibility(View.VISIBLE);
+        vh.rightText.setVisibility(View.VISIBLE);
         vh.backgroundImage.setImageResource(R.drawable.choice_point_background_center);
     }
 }
